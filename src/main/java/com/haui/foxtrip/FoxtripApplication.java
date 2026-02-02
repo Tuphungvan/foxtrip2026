3f05c1d5bc6b025c32e25106806b60a797d1c3d7
Tuphungvan <cube438126@gmail.com>
2026-02-02 10:15:40 +0700

    first commit

---
+++ src/main/java/com/haui/foxtrip/FoxtripApplication.java
@@ -0,0 +1,13 @@
+package com.haui.foxtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxtripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haui/foxtrip/FoxtripApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.haui.foxtrip;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class FoxtripApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
